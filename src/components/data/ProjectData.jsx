fa8a9f2da18e30eb1d8ef8ff98cfae5d2b881dab
FelixWahyu <felixwahyusejati89@gmail.com>
2025-12-04 11:22:37 +0700

    update deploy portofolio

--- src/components/data/ProjectData.jsx
+++ src/components/data/ProjectData.jsx
@@ -1,16 +1,19 @@
+import KasirImg from "../../assets/project/project-kasir-web.png";
+import PenjualanImg from "../../assets/project/sistem-web-penjualan.png";
+
 export const ProjectsData = [
   {
     title: "Sistem Web Kasir Toko Kelontong",
     description: "A full-stack system web chasier solution with integration AI Gemini, Discount member management, and analytics dashboard.",
     tech: ["Laravel", "Blade", "TailwindCSS", "JavaScript", "AI Gemini", "MySql"],
     link: "https://github.com/FelixWahyu/website-kasir-laravel.git",
-    image: "/project/project-kasir-web.png",
+    image: KasirImg,
   },
   {
     title: "Sistem Web Penjualan Toko Roti",
     description: "A web-based sales application with Laravel Blade and AlpineJs, featuring payment integration and natural language processing.",
     tech: ["Laravel", "Blade", "AlpineJs", "TailwindCSS", "MySql"],
     link: "https://github.com/FelixWahyu/toko-roti-mruyung.git",
-    image: "/project/sistem-web-penjualan.png",
+    image: PenjualanImg,
   },
 ];
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/my-portofolio-website",
+  base: "/my-portofolio-website/",
 });
